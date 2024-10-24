59a52b0c5c437faf072afe68bda7dcaa884eeea1
BoschSahana17 <167978516+BoschSahana17@users.noreply.github.com>
2024-10-24 11:49:01 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,70 +1,47 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoundexTestSuite, HandlesEmptyInput) {
-    char soundex[5];
-    
-    generateSoundex("", soundex);  // Empty input
-    ASSERT_STREQ(soundex, "");
+#define MAX_CODE_LEN 5
+
+char soundex[MAX_CODE_LEN];
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+  generateSoundex("AX", soundex);
+ EXPECT_STREQ(soundex,"A200");
+ // ASSERT_EQ(soundex,"A200");
 }
 
-TEST(SoundexTestSuite, HandlesSingleConsonant) {
-    char soundex[5];
-    
-    generateSoundex("B", soundex);  // Single consonant
-    ASSERT_STREQ(soundex, "B000");
+TEST(SoudexTestsuite, ReplaceLowerCaseToUpeerCaseAndEncode) {
+  generateSoundex("kashi", soundex);
+ EXPECT_STREQ(soundex,"K200");
 }
 
-TEST(SoundexTestSuite, HandlesOnlyVowels) {
-    char soundex[5];
-    
-    generateSoundex("AEIOU", soundex);  // Only vowels
-    ASSERT_STREQ(soundex, "A000");
+TEST(SoudexTestsuite, PadsWithZeoIfStringIsLessThanFourCharacters) {
+  generateSoundex("kk", soundex);
+  EXPECT_STREQ(soundex,"K200");
 }
 
-TEST(SoundexTestSuite, IgnoresCaseDifferences) {
-    char soundex[5];
-
-    generateSoundex("Smith", soundex);  // Mixed case input
-    ASSERT_STREQ(soundex, "S530");
-
-    generateSoundex("sMiTh", soundex);  // Different case, same expected result
-    ASSERT_STREQ(soundex, "S530");
+TEST(SoudexTestsuite, HandleEmptyString) {
+  generateSoundex("", soundex);
+  EXPECT_STREQ(soundex,"");
 }
 
-TEST(SoundexTestSuite, HandlesRepeatedConsonants) {
-    char soundex[5];
-    
-    generateSoundex("Bbbb", soundex);  // Repeated consonants
-    ASSERT_STREQ(soundex, "B100");
-
-    generateSoundex("Ddddd", soundex);  // Another repeated consonant
-    ASSERT_STREQ(soundex, "D300");
+TEST(SoudexTestsuite, IgnoreDuplicateCharactersAndEncod) {
+  generateSoundex("FFFF", soundex);
+  EXPECT_STREQ(soundex,"F100");
 }
 
-TEST(SoundexTestSuite, TruncatesLongNames) {
-    char soundex[5];
-    
-    generateSoundex("Washington", soundex);  // Name longer than 4 characters
-    ASSERT_STREQ(soundex, "W252");
-
-    generateSoundex("Gutierrez", soundex);  // Another long name
-    ASSERT_STREQ(soundex, "G362");
+TEST(SoudexTestsuite, HandleSpeacialCharacter) {
+  generateSoundex("@!%#", soundex);
+  EXPECT_STREQ(soundex,"@000");
 }
 
-TEST(SoundexTestSuite, HandlesNameWithMultipleZeroMappedLetters) {
-    char soundex[5];
-    
-    generateSoundex("Ashcraft", soundex);  // 'H' and 'C' get mapped to zero
-    ASSERT_STREQ(soundex, "A261");
+TEST(SoudexTestsuite, ReplaceCharactersByNumbers) {
+  generateSoundex("1ABC", soundex);
+  EXPECT_STREQ(soundex,"1200");
 }
 
-TEST(SoundexTestSuite, PadsShortNamesWithZeros) {
-    char soundex[5];
-    
-    generateSoundex("Li", soundex);  // Short name, needs padding
-    ASSERT_STREQ(soundex, "L000");
-
-    generateSoundex("Ga", soundex);  // Another short name
-    ASSERT_STREQ(soundex, "G000");
+TEST(SoudexTestsuite, IgnoreOtherCharactersIfStringLengthIsMoreThanMaxCodeLength) {
+  generateSoundex("abcdefefg", soundex);
+  EXPECT_STREQ(soundex,"A123");
 }
