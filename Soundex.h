82ac3fa0cba89913a562d49466c9893cf26f2dfc
BoschSahana17 <167978516+BoschSahana17@users.noreply.github.com>
2024-10-24 11:49:25 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,43 +1,65 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
- 
+
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
- 
+
+// Get the code from Table
+char getSoundexCodeFromTable(char c, const char *soundexTable)
+{
+    c = toupper(c);
+    if (c >= 'A' && c <= 'Z') {
+        return soundexTable[c - 'A'];
+    }
+    return '0'; // For any character outside A-Z
+}
+
 char getSoundexCode(char c) {
-static const char soundexMap[26] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', 
-        '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
+    // Update the table with A to Z characters by numbers 
+    static const char soundexTable[26] = {
+        '0', '1', '2', '3', '0', '1', '2', '0', // A-H
+        '0', '2', '2', '4', '5', '5', '0', '1', // I-P
+        '2', '6', '2', '3', '0', '1', '0', '2', // Q-X
+        '0', '2'                                // Y-Z
     };
-    return soundexMap[toupper(c) - 'A'];} // Map uppercase letter 'c' to its Soundex code.
- 
-void processChar(char name, char *soundex,int *index){
-     char code = getSoundexCode(name);
-     if (code != '0' && code != soundex[*index - 1]) {
-         soundex[(*index)++] = code;
-     } 
+    
+    return getSoundexCodeFromTable(c, soundexTable);
+    
 }
- 
-void handleName(const char *name, int *index, int len, char *soundex) {
-    for (int i = 1; i < len && *index < 4; i++) {
-        processChar(name[i],soundex,index);
-    }    
-}
- 
-void padWithZeros(int sIndex, char *soundex) {
-        while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+
+void validateCode(char code, char *soundex, int *sIndex)
+{
+    if (code != '0' && code != soundex[*sIndex - 1]) {
+            soundex[(*sIndex)++] = code;
     }
 }
- 
- 
-void generateSoundex(const char *name, char *soundex) {
+
+void processSoundex(const char *name, char *soundex, int *sIndex)
+{
     int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-    handleName(name,&sIndex,len,soundex);
-    padWithZeros(sIndex,soundex);
+    for (int i = 1; i < len && *sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        validateCode(code, soundex, sIndex);
+    }
+}
+
+void padsWithZero(char *soundex, int sIndex)
+{
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
+    }
+
     soundex[4] = '\0';
 }
+
+void generateSoundex(const char *name, char *soundex) {
+    soundex[0] = toupper(name[0]);
+    int sIndex = 1;
+
+    processSoundex(name, soundex, &sIndex);
+    
+    padsWithZero(soundex, sIndex);
+}
+
 #endif // SOUNDEX_H
